1ebfd8e2cd702d54f4e65cc72cdab17a3456b24e
buyunwang <buyunwang@hotmail.com>
2020-12-30 22:18:43 -0800

    Update App.tsx

--- RNApp/App.tsx
+++ RNApp/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [state, setState] = useState('')
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
