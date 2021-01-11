d1c5215805f4599842e1e0feb36db76e12f19700
buyunwang <wby920923@gmail.com>
2021-01-10 18:36:54 -0800

    Create home.tsx

---
+++ RNApp/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const HomePage = props => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+}
+
+export default HomePage
